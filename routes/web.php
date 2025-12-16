9c4ebb60c3f83ae80c1911d503fec70030ec5f6c
Lucas Lannes <lucas.lannes@wwlucas.dev>
2025-12-16 08:59:31 -0300

    normalized routes and tests. Removed dashboard feats

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
 require __DIR__.'/webapp.php';
 
 Route::get('/', function () {
-    return "Website";
-})->name('home');
+    return 'Website';
+})->name('home')->middleware(['auth', 'verified']);
 
 require __DIR__.'/settings.php';
--- routes/webapp.php
+++ routes/webapp.php
@@ -7,8 +7,10 @@ $domain = str_replace('http://', '', config('app.url'));
 
 Route::domain("app.$domain")->group(function () {
     Route::middleware(['auth', 'verified'])->group(function () {
+
         Route::get('/', function () {
             return Inertia::render('webapp/home');
-        });
+        })->name('webapp.home');
+
     });
 });
