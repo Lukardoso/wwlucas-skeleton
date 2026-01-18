72a06bd38e6da4bcfea35f848373e459d4e0ba21
Lucas Lannes <lucas.lannes@wwlucas.dev>
2026-01-18 17:13:16 -0300

    Remove phpstan from tests path. Not worth it

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -3,28 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class AvatarController extends Controller
 {
-    public function show(User $user)
+    public function show(User $user): StreamedResponse
     {
         abort_if($user->id !== Auth::id(), 403);
+
+        abort_if(! $user->avatar, 404, "Avatar not found.");
 
         return Storage::response(
             $user->avatar,
         );
     }
 
-    public function update(Request $request)
+    public function update(Request $request): RedirectResponse
     {
         $request->validate([
             'avatar' => ['required', 'image'],
         ]);
 
-        $request->user()->update([
+        $request->user()?->update([
             'avatar' => $request->file('avatar')->store('avatars'),
         ]);
 
--- tests/Feature/Http/AvatarTest.php
+++ tests/Feature/Http/AvatarTest.php
@@ -1,3 +1,19 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+test('avatar can be updated', function () {
+    Storage::fake('local');
+
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->put(route('avatar.update', $user->id), [
+        'avatar' => UploadedFile::fake()->image('avatar.jpg'),
+    ]);
+
+    $response->assertRedirect();
+
+    Storage::disk('local')->assertExists($user->avatar);
+});
