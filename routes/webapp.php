bac625e1e809eb70c357c2cf97e7941660d2f71d
Lucas Lannes <lucas.lannes@wwlucas.dev>
2026-01-26 19:30:16 -0300

    Refactored examples on webapp route

--- routes/webapp.php
+++ routes/webapp.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AvatarController;
 use App\Http\Middleware\EnsureHasPermission;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 $domain = str_replace('http://', '', config('app.url'));
@@ -23,22 +24,26 @@ Route::domain("app.$domain")->group(function () {
         Route::get('/fake-api/{search}', function (string $search) {
             sleep(1);
 
-            return str_contains($search, 'j')
-                ? response()->json([
-                    [
-                        'id' => 1,
-                        'name' => 'John Doe',
-                        'email' => 'dDkYH@example.com',
-                        'address' => 'Some fake address 78 street',
-                    ],
-                    [
-                        'id' => 2,
-                        'name' => 'Jane Doe',
-                        'email' => 'dDkYH@example.com',
-                        'address' => 'Some fake address 78 street',
-                    ],
-                ])
-                : response()->json([]);
+            $data = collect([
+                [
+                    'id' => 1,
+                    'name' => 'Jane Doe',
+                    'email' => 'email@example.com',
+                    'address' => 'Some fake address 78 street',
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'John Doe',
+                    'email' => 'email@example.com',
+                    'address' => 'Some fake address 78 street',
+                ],
+            ]);
+
+            $filtered = $data->filter(fn ($item) => Str::contains($item['name'], $search, true) ||
+                Str::contains($item['email'], $search, true)
+            )->values();
+
+            return response()->json($filtered);
         });
 
         Route::get('/avatar/{user}', [AvatarController::class, 'show'])->name('avatar.show');
